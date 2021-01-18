b7375518c8ca43a5e3d2b427b46d38e3bedabfc2
Wei-Ming Lin <c7934597@gmail.com>
2021-01-18 14:21:13 +0800

    updatw

--- README.md
+++ README.md
@@ -7,13 +7,12 @@ source /home/minggatsby/python/bin/activate
 2.
 cd src/Anti-Candid/
 
-
-
 3.
 Object Detection
 
 If it use gstreamer
 python3 trt_yolo.py -m yolov4-416 --usb 0
+
 If it doesn't use gstreamer
 python3 trt_yolo.py -m yolov4-416 --usb 0 --gstreamer false 
 
@@ -21,10 +20,13 @@ python3 trt_yolo.py -m yolov4-416 --usb 0 --gstreamer false
 Pose_Estimation
 
 cd src/Anti-Candid/trt_pose/tasks/human_pose/
-
-python3 start.py
+python3 detect_camera.py
 
 
 Object Detection & Pose_Estimation
 
-python3 detect_camera.py --usb 0 -g 
+If it use gstreamer
+python3 main.py --usb 0
+
+If it doesn't use gstreamer
+python3 main.py --usb 0 -g 
--- main.py
+++ main.py
@@ -15,58 +15,49 @@ from trt_pose.parse_objects import ParseObjects
 import argparse
 import os.path
 
-"""Parse input arguments."""
-parser = argparse.ArgumentParser(description='TensorRT pose estimation run')
-parser.add_argument('--model', type=str, default='resnet', help = 'resnet or densenet' )
-parser.add_argument('--video', type=str, default='/home/minggatsby/src/test_images/video.avi', help = 'video file name' )
-args = parser.parse_args()
+import os
+import time
+import argparse
 
-with open('./trt_pose/tasks/human_pose/human_pose.json', 'r') as f:
-    human_pose = json.load(f)
+import cv2
+import pycuda.autoinit  # This is needed for initializing CUDA driver
 
-topology = trt_pose.coco.coco_category_to_topology(human_pose)
+from utils.yolo_classes import get_cls_dict
+from utils.camera import add_camera_args, Camera
+from utils.display import open_window, set_display, show_fps
+from utils.visualization import BBoxVisualization
 
-num_parts = len(human_pose['keypoints'])
-num_links = len(human_pose['skeleton'])
+from utils.yolo_with_plugins import TrtYOLO
 
-if 'resnet' in args.model:
-    print('------ model = resnet--------')
-    MODEL_WEIGHTS = './trt_pose/tasks/human_pose/resnet18_baseline_att_224x224_A_epoch_249.pth'
-    OPTIMIZED_MODEL = './trt_pose/tasks/human_pose/resnet18_baseline_att_224x224_A_epoch_249_trt.pth'
-    model = trt_pose.models.resnet18_baseline_att(num_parts, 2 * num_links).cuda().eval()
-    WIDTH = 224
-    HEIGHT = 224
 
-else:    
-    print('------ model = densenet--------')
-    MODEL_WEIGHTS = './trt_pose/tasks/human_pose/densenet121_baseline_att_256x256_B_epoch_160.pth'
-    OPTIMIZED_MODEL = './trt_pose/tasks/human_pose/densenet121_baseline_att_256x256_B_epoch_160_trt.pth'
-    model = trt_pose.models.densenet121_baseline_att(num_parts, 2 * num_links).cuda().eval()
-    WIDTH = 256
-    HEIGHT = 256
+def parse_args():
+    """Parse input arguments."""
+    desc = ('Capture and display live camera video, while doing '
+            'real-time object detection with TensorRT optimized '
+            'YOLO model on Jetson'
+           'TensorRT pose estimation run')
+    parser = argparse.ArgumentParser(description=desc)
+    parser = add_camera_args(parser)
+    parser.add_argument(
+        '-c', '--category_num', type=int, default=1,
+        help='number of object categories [1')
+    parser.add_argument(
+        '-ym', '--yolo_model', type=str, default='yolov4-416', 
+        help=('[yolov3|yolov3-tiny|yolov3-spp|yolov4|yolov4-tiny]-'
+              '[{dimension}], where dimension could be a single '
+              'number (e.g. 288, 416, 608) or WxH (e.g. 416x256)'))
+    parser.add_argument(
+        '-pm', '--pose_model', type=str, default='resnet', 
+        help = 'resnet or densenet')
+    parser.add_argument(
+        '-l', '--letter_box', action='store_true',
+        help='inference with letterboxed image [False]')
+    parser.add_argument(
+        '-g', '--gstreamer', action='store_false',
+        help='inference with gstreamer [True]')
+    args = parser.parse_args()
+    return args
 
-data = torch.zeros((1, 3, HEIGHT, WIDTH)).cuda()
-if os.path.exists(OPTIMIZED_MODEL) == False:
-    print('-- Converting TensorRT models. This may takes several minutes...')
-    model.load_state_dict(torch.load(MODEL_WEIGHTS))
-    model_trt = torch2trt.torch2trt(model, [data], fp16_mode=True, max_workspace_size=1<<25)
-    torch.save(model_trt.state_dict(), OPTIMIZED_MODEL)
-
-model_trt = TRTModule()
-model_trt.load_state_dict(torch.load(OPTIMIZED_MODEL))
-
-t0 = time.time()
-torch.cuda.current_stream().synchronize()
-for i in range(50):
-    y = model_trt(data)
-torch.cuda.current_stream().synchronize()
-t1 = time.time()
-
-print(50.0 / (t1 - t0))
-
-mean = torch.Tensor([0.485, 0.456, 0.406]).cuda()
-std = torch.Tensor([0.229, 0.224, 0.225]).cuda()
-device = torch.device('cuda')
 
 def draw_keypoints(img, key):
     thickness = 5
@@ -133,6 +124,7 @@ def draw_keypoints(img, key):
         draw.line([ round(key[0][2] * w), round(key[0][1] * h), round(key[17][2] * w), round(key[17][1] * h)],width = thickness, fill=(255,255,0))
     return img
 
+
 '''
 hnum: 0 based human index
 kpoint : keypoints (float type range : 0.0 ~ 1.0 ==> later multiply by image width, height
@@ -155,6 +147,7 @@ def get_keypoint(humans, hnum, peaks):
             #print('index:%d : None %d'%(j, k) )
     return kpoint
 
+
 def preprocess(image):
     global device
     device = torch.device('cuda')
@@ -163,6 +156,7 @@ def preprocess(image):
     image = transforms.functional.to_tensor(image).to(device)
     image.sub_(mean[:, None, None]).div_(std[:, None, None])
     return image[None, ...]
+ 
     
 '''
 Draw to original image
@@ -186,34 +180,134 @@ def execute(img, org, count):
     print('===== Frmae[%d] Net FPS :%f ====='%(count, netfps))
     return org
 
-'''
-v4l2-ctl --list-formats-ext指令查詢規範
-sudo systemctl restart nvargus-daemon指令重啟相機服務
-'''
-_WIDTH=640
-_HEIGHT=480
-_FLIP_METHOD=0
 
-# 選擇相機 MIPI / USB
-# camSet = 'nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=3264, height=2464, framerate=21/1, format=NV12 ! nvvidconv flip-method='+str(_FLIP_METHOD)+' ! video/x-raw, width='+str(_WIDTH)+', height='+str(_HEIGHT)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
-# camSet = 'v4l2src device=/dev/video0 ! video/x-raw, width='+str(_WIDTH)+', height='+str(_HEIGHT)+', framerate=24/1 ! videoconvert ! appsink'
+def loop_and_detect(cam, trt_yolo, conf_th, vis):
+    """Continuously capture images from camera and do object detection.
 
-# cap = cv2.VideoCapture(camSet)
-cap = cv2.VideoCapture(0)
-# cap = cv2.VideoCapture(args.video)
+    # Arguments
+      cam: the camera instance (video source).
+      trt_yolo: the TRT YOLO object detector instance.
+      conf_th: confidence/score threshold for object detection.
+      vis: for visualization.
+    """
+    full_scrn = False
+    fps = 0.0
+    tic = time.time()
+    count = 1
+    while True:
+        if cv2.getWindowProperty(WINDOW_NAME, 0) < 0:
+            break
+        dst = cam.read()
+        if dst is None:
+            print("Frame Read End")
+            break           
+            
+        img = cv2.resize(dst, dsize=(WIDTH, HEIGHT), interpolation=cv2.INTER_AREA)
+        pilimg = PIL.Image.fromarray(dst)
+        pilimg = execute(img, pilimg, count)
+        img = np.asarray(pilimg, dtype="uint8")
+        count += 1
+            
+        boxes, confs, clss = trt_yolo.detect(img, conf_th)
+        img = vis.draw_bboxes(img, boxes, confs, clss)
+        img = show_fps(img, fps)
+        cv2.imshow(WINDOW_NAME, img)
+        toc = time.time()
+        curr_fps = 1.0 / (toc - tic)
+        # calculate an exponentially decaying average of fps number
+        fps = curr_fps if fps == 0.0 else (fps*0.95 + curr_fps*0.05)
+        tic = toc
+        key = cv2.waitKey(1)
+        if key == 27:  # ESC key: quit program
+            break
+        elif key == ord('F') or key == ord('f'):  # Toggle fullscreen
+            full_scrn = not full_scrn
+            set_display(WINDOW_NAME, full_scrn)
+            
+            
+def main():
+    if args.category_num <= 0:
+        raise SystemExit('ERROR: bad category_num (%d)!' % args.category_num)
+    if not os.path.isfile('yolo/%s.trt' % args.yolo_model):
+        raise SystemExit('ERROR: file (yolo/%s.trt) not found!' % args.yolo_model)
 
-ret_val, img = cap.read()
-# H, W, __ = img.shape
+    cam = Camera(args)
+    if not cam.isOpened():
+        raise SystemExit('ERROR: failed to open camera!')
 
-# fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-# dir, filename = os.path.split(args.video)
-# name, ext = os.path.splitext(filename)
-# out_video = cv2.VideoWriter('/home/minggatsby/src/test_images/result/%s_%s.mp4'%(args.model, name), fourcc, cap.get(cv2.CAP_PROP_FPS), (W, H))
-# count = 0
+    cls_dict = get_cls_dict(args.category_num)
+    yolo_dim = args.yolo_model.split('-')[-1]
+    if 'x' in yolo_dim:
+        dim_split = yolo_dim.split('x')
+        if len(dim_split) != 2:
+            raise SystemExit('ERROR: bad yolo_dim (%s)!' % yolo_dim)
+        w, h = int(dim_split[0]), int(dim_split[1])
+    else:
+        h = w = int(yolo_dim)
+    if h % 32 != 0 or w % 32 != 0:
+        raise SystemExit('ERROR: bad yolo_dim (%s)!' % yolo_dim)
 
-if cap is None:
-    print("Camera Open Error")
-    sys.exit(0)
+    trt_yolo = TrtYOLO(args.yolo_model, (h, w), args.category_num, args.letter_box)
+
+    open_window(
+        WINDOW_NAME, 'Camera TensorRT YOLO Demo',
+        cam.img_width, cam.img_height)
+    vis = BBoxVisualization(cls_dict)
+    loop_and_detect(cam, trt_yolo, conf_th=0.3, vis=vis)
+
+    cam.release()
+    cv2.destroyAllWindows()
+    
+           
+WINDOW_NAME = 'Demo'
+args = parse_args()
+
+with open('./trt_pose/tasks/human_pose/human_pose.json', 'r') as f:
+    human_pose = json.load(f)
+
+topology = trt_pose.coco.coco_category_to_topology(human_pose)
+
+num_parts = len(human_pose['keypoints'])
+num_links = len(human_pose['skeleton'])
+
+if 'resnet' in args.pose_model:
+    print('------ model = resnet--------')
+    MODEL_WEIGHTS = './trt_pose/tasks/human_pose/resnet18_baseline_att_224x224_A_epoch_249.pth'
+    OPTIMIZED_MODEL = './trt_pose/tasks/human_pose/resnet18_baseline_att_224x224_A_epoch_249_trt.pth'
+    model = trt_pose.models.resnet18_baseline_att(num_parts, 2 * num_links).cuda().eval()
+    WIDTH = 224
+    HEIGHT = 224
+
+else:    
+    print('------ model = densenet--------')
+    MODEL_WEIGHTS = './trt_pose/tasks/human_pose/densenet121_baseline_att_256x256_B_epoch_160.pth'
+    OPTIMIZED_MODEL = './trt_pose/tasks/human_pose/densenet121_baseline_att_256x256_B_epoch_160_trt.pth'
+    model = trt_pose.models.densenet121_baseline_att(num_parts, 2 * num_links).cuda().eval()
+    WIDTH = 256
+    HEIGHT = 256
+
+data = torch.zeros((1, 3, HEIGHT, WIDTH)).cuda()
+if os.path.exists(OPTIMIZED_MODEL) == False:
+    print('-- Converting TensorRT models. This may takes several minutes...')
+    model.load_state_dict(torch.load(MODEL_WEIGHTS))
+    model_trt = torch2trt.torch2trt(model, [data], fp16_mode=True, max_workspace_size=1<<25)
+    torch.save(model_trt.state_dict(), OPTIMIZED_MODEL)
+
+model_trt = TRTModule()
+model_trt.load_state_dict(torch.load(OPTIMIZED_MODEL))
+
+t0 = time.time()
+torch.cuda.current_stream().synchronize()
+for i in range(50):
+    y = model_trt(data)
+torch.cuda.current_stream().synchronize()
+t1 = time.time()
+
+print(50.0 / (t1 - t0))
+
+mean = torch.Tensor([0.485, 0.456, 0.406]).cuda()
+std = torch.Tensor([0.229, 0.224, 0.225]).cuda()
+device = torch.device('cuda')
 
 parse_objects = ParseObjects(topology)
 draw_objects = DrawObjects(topology)
@@ -221,29 +315,6 @@ draw_objects = DrawObjects(topology)
 fontname = '/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc'
 fnt = PIL.ImageFont.truetype(fontname, 45)
 
-count = 1
-while cap.isOpened():
-    ret_val, dst = cap.read()
-    if ret_val == False:
-        print("Frame Read End")
-        break       
-    img = cv2.resize(dst, dsize=(WIDTH, HEIGHT), interpolation=cv2.INTER_AREA)
-#     pilimg = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
-#     pilimg = PIL.Image.fromarray(pilimg)
-    pilimg = PIL.Image.fromarray(dst)
-    pilimg = execute(img, pilimg, count)
-    array = np.asarray(pilimg, dtype="uint8")
-    # out_video.write(array)
-    count += 1
 
-    # 顯示影像
-#     cv2.imshow('myCam', dst) # 顯示原始影像
-    cv2.imshow('myCam', array) # 顯示姿態估計影像
-
-    # 若按下 q 鍵則離開迴圈
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-cv2.destroyAllWindows()
-# out_video.release()
-cap.release()
+if __name__ == '__main__':
+    main() 
