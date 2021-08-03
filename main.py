32ed98ef22b85fc03b789cdc92d190e874f7e604
Wei-Ming Lin <c7934597@gmail.com>
2021-08-03 16:53:00 +0800

    update

---
+++ main.py
@@ -0,0 +1,323 @@
+import json
+import trt_pose.coco
+import trt_pose.models
+import torch
+import torch2trt
+from torch2trt import TRTModule
+import time, sys
+import cv2
+import PIL.Image, PIL.ImageDraw, PIL.ImageFont
+import numpy as np
+
+import torchvision.transforms as transforms
+from trt_pose.draw_objects import DrawObjects
+from trt_pose.parse_objects import ParseObjects
+import argparse
+import os.path
+
+import os
+import time
+import argparse
+
+import cv2
+import pycuda.autoinit  # This is needed for initializing CUDA driver
+
+from utils.yolo_classes import get_cls_dict
+from utils.camera import add_camera_args, Camera
+from utils.display import open_window, set_display, show_fps
+from utils.visualization import BBoxVisualization
+
+from utils.yolo_with_plugins import TrtYOLO
+
+
+def parse_args():
+    """Parse input arguments."""
+    desc = ('Capture and display live camera video, while doing '
+            'real-time object detection with TensorRT optimized '
+            'YOLO model on Jetson'
+           'TensorRT pose estimation run')
+    parser = argparse.ArgumentParser(description=desc)
+    parser = add_camera_args(parser)
+    parser.add_argument(
+        '-c', '--category_num', type=int, default=80,
+        help='number of object categories [80]')
+    parser.add_argument(
+        '-ym', '--yolo_model', type=str, default='yolov4-tiny-416', 
+        help=('[yolov3|yolov3-tiny|yolov3-spp|yolov4|yolov4-tiny]-'
+              '[{dimension}], where dimension could be a single '
+              'number (e.g. 288, 416, 608) or WxH (e.g. 416x256)'))
+    parser.add_argument(
+        '-pm', '--pose_model', type=str, default='resnet', 
+        help = 'resnet or densenet')
+    parser.add_argument(
+        '-l', '--letter_box', action='store_true',
+        help='inference with letterboxed image [False]')
+    parser.add_argument(
+        '-g', '--gstreamer', action='store_false',
+        help='inference with gstreamer [True]')
+    args = parser.parse_args()
+    return args
+
+
+def draw_keypoints(img, key):
+    thickness = 5
+    w, h = img.size
+    draw = PIL.ImageDraw.Draw(img)
+    #draw Rankle -> RKnee (16-> 14)
+    if all(key[16]) and all(key[14]):
+        draw.line([ round(key[16][2] * w), round(key[16][1] * h), round(key[14][2] * w), round(key[14][1] * h)],width = thickness, fill=(51,51,204))
+    #draw RKnee -> Rhip (14-> 12)
+    if all(key[14]) and all(key[12]):
+        draw.line([ round(key[14][2] * w), round(key[14][1] * h), round(key[12][2] * w), round(key[12][1] * h)],width = thickness, fill=(51,51,204))
+    #draw Rhip -> Lhip (12-> 11)
+    if all(key[12]) and all(key[11]):
+        draw.line([ round(key[12][2] * w), round(key[12][1] * h), round(key[11][2] * w), round(key[11][1] * h)],width = thickness, fill=(51,51,204))
+    #draw Lhip -> Lknee (11-> 13)
+    if all(key[11]) and all(key[13]):
+        draw.line([ round(key[11][2] * w), round(key[11][1] * h), round(key[13][2] * w), round(key[13][1] * h)],width = thickness, fill=(51,51,204))
+    #draw Lknee -> Lankle (13-> 15)
+    if all(key[13]) and all(key[15]):
+        draw.line([ round(key[13][2] * w), round(key[13][1] * h), round(key[15][2] * w), round(key[15][1] * h)],width = thickness, fill=(51,51,204))
+
+    #draw Rwrist -> Relbow (10-> 8)
+    if all(key[10]) and all(key[8]):
+        draw.line([ round(key[10][2] * w), round(key[10][1] * h), round(key[8][2] * w), round(key[8][1] * h)],width = thickness, fill=(255,255,51))
+    #draw Relbow -> Rshoulder (8-> 6)
+    if all(key[8]) and all(key[6]):
+        draw.line([ round(key[8][2] * w), round(key[8][1] * h), round(key[6][2] * w), round(key[6][1] * h)],width = thickness, fill=(255,255,51))
+    #draw Rshoulder -> Lshoulder (6-> 5)
+    if all(key[6]) and all(key[5]):
+        draw.line([ round(key[6][2] * w), round(key[6][1] * h), round(key[5][2] * w), round(key[5][1] * h)],width = thickness, fill=(255,255,0))
+    #draw Lshoulder -> Lelbow (5-> 7)
+    if all(key[5]) and all(key[7]):
+        draw.line([ round(key[5][2] * w), round(key[5][1] * h), round(key[7][2] * w), round(key[7][1] * h)],width = thickness, fill=(51,255,51))
+    #draw Lelbow -> Lwrist (7-> 9)
+    if all(key[7]) and all(key[9]):
+        draw.line([ round(key[7][2] * w), round(key[7][1] * h), round(key[9][2] * w), round(key[9][1] * h)],width = thickness, fill=(51,255,51))
+
+    #draw Rshoulder -> RHip (6-> 12)
+    if all(key[6]) and all(key[12]):
+        draw.line([ round(key[6][2] * w), round(key[6][1] * h), round(key[12][2] * w), round(key[12][1] * h)],width = thickness, fill=(153,0,51))
+    #draw Lshoulder -> LHip (5-> 11)
+    if all(key[5]) and all(key[11]):
+        draw.line([ round(key[5][2] * w), round(key[5][1] * h), round(key[11][2] * w), round(key[11][1] * h)],width = thickness, fill=(153,0,51))
+
+
+    #draw nose -> Reye (0-> 2)
+    if all(key[0][1:]) and all(key[2]):
+        draw.line([ round(key[0][2] * w), round(key[0][1] * h), round(key[2][2] * w), round(key[2][1] * h)],width = thickness, fill=(219,0,219))
+
+    #draw Reye -> Rear (2-> 4)
+    if all(key[2]) and all(key[4]):
+        draw.line([ round(key[2][2] * w), round(key[2][1] * h), round(key[4][2] * w), round(key[4][1] * h)],width = thickness, fill=(219,0,219))
+
+    #draw nose -> Leye (0-> 1)
+    if all(key[0][1:]) and all(key[1]):
+        draw.line([ round(key[0][2] * w), round(key[0][1] * h), round(key[1][2] * w), round(key[1][1] * h)],width = thickness, fill=(219,0,219))
+
+    #draw Leye -> Lear (1-> 3)
+    if all(key[1]) and all(key[3]):
+        draw.line([ round(key[1][2] * w), round(key[1][1] * h), round(key[3][2] * w), round(key[3][1] * h)],width = thickness, fill=(219,0,219))
+
+    #draw nose -> neck (0-> 17)
+    if all(key[0][1:]) and all(key[17]):
+        draw.line([ round(key[0][2] * w), round(key[0][1] * h), round(key[17][2] * w), round(key[17][1] * h)],width = thickness, fill=(255,255,0))
+    return img
+
+
+'''
+hnum: 0 based human index
+kpoint : keypoints (float type range : 0.0 ~ 1.0 ==> later multiply by image width, height
+'''
+def get_keypoint(humans, hnum, peaks):
+    #check invalid human index
+    kpoint = []
+    human = humans[0][hnum]
+    C = human.shape[0]
+    for j in range(C):
+        k = int(human[j])
+        if k >= 0:
+            peak = peaks[0][j][k]   # peak[1]:width, peak[0]:height
+            peak = (j, float(peak[0]), float(peak[1]))
+            kpoint.append(peak)
+            #print('index:%d : success [%5.3f, %5.3f]'%(j, peak[1], peak[2]) )
+        else:    
+            peak = (j, None, None)
+            kpoint.append(peak)
+            #print('index:%d : None %d'%(j, k) )
+    return kpoint
+
+
+def preprocess(image):
+    global device
+    device = torch.device('cuda')
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = PIL.Image.fromarray(image)
+    image = transforms.functional.to_tensor(image).to(device)
+    image.sub_(mean[:, None, None]).div_(std[:, None, None])
+    return image[None, ...]
+ 
+
+'''
+Draw to original image
+'''
+def execute(img, org, count):
+    #start = time.time()
+    data = preprocess(img)
+    cmap, paf = model_trt(data)
+    cmap, paf = cmap.detach().cpu(), paf.detach().cpu()
+    #end = time.time()
+    counts, objects, peaks = parse_objects(cmap, paf)#, cmap_threshold=0.15, link_threshold=0.15)
+    for i in range(counts[0]):
+        #print("Human index:%d "%( i ))
+        kpoint = get_keypoint(objects, i, peaks)
+        #print(kpoint)
+        org = draw_keypoints(org, kpoint)
+    #netfps = 1 / (end - start)  
+    # draw = PIL.ImageDraw.Draw(org)
+    #draw.text((30, 30), "NET FPS:%4.1f"%netfps, font=fnt, fill=(0,255,0))    
+    print("Human count:%d len:%d "%(counts[0], len(counts)))
+    #print('===== Frmae[%d] Net FPS :%f ====='%(count, netfps))
+    return org
+
+
+def loop_and_detect(cam, trt_yolo, conf_th, vis):
+    """Continuously capture images from camera and do object detection.
+
+    # Arguments
+      cam: the camera instance (video source).
+      trt_yolo: the TRT YOLO object detector instance.
+      conf_th: confidence/score threshold for object detection.
+      vis: for visualization.
+    """
+    full_scrn = False
+    fps = 0.0
+    tic = time.time()
+    count = 1
+    while True:
+        if cv2.getWindowProperty(WINDOW_NAME, 0) < 0:
+            break
+        dst = cam.read()
+        if dst is None:
+            print("Frame Read End")
+            break           
+            
+        img = cv2.resize(dst, dsize=(WIDTH, HEIGHT), interpolation=cv2.INTER_AREA)
+        pilimg = PIL.Image.fromarray(dst)
+
+        boxes, confs, clss = trt_yolo.detect(dst, conf_th)
+
+        pilimg = execute(img, pilimg, count)
+        img = np.asarray(pilimg, dtype="uint8")
+        count += 1
+    
+        img = vis.draw_bboxes(img, boxes, confs, clss)
+
+        img = show_fps(img, fps)
+        cv2.imshow(WINDOW_NAME, img)
+        toc = time.time()
+        curr_fps = 1.0 / (toc - tic)
+        # calculate an exponentially decaying average of fps number
+        fps = curr_fps if fps == 0.0 else (fps*0.95 + curr_fps*0.05)
+        tic = toc
+        key = cv2.waitKey(1)
+        if key == 27:  # ESC key: quit program
+            break
+        elif key == ord('F') or key == ord('f'):  # Toggle fullscreen
+            full_scrn = not full_scrn
+            set_display(WINDOW_NAME, full_scrn)
+
+
+def main():
+    if args.category_num <= 0:
+        raise SystemExit('ERROR: bad category_num (%d)!' % args.category_num)
+    if not os.path.isfile('yolo/%s.trt' % args.yolo_model):
+        raise SystemExit('ERROR: file (yolo/%s.trt) not found!' % args.yolo_model)
+
+    cam = Camera(args)
+    if not cam.isOpened():
+        raise SystemExit('ERROR: failed to open camera!')
+
+    cls_dict = get_cls_dict(args.category_num)
+    yolo_dim = args.yolo_model.split('-')[-1]
+    if 'x' in yolo_dim:
+        dim_split = yolo_dim.split('x')
+        if len(dim_split) != 2:
+            raise SystemExit('ERROR: bad yolo_dim (%s)!' % yolo_dim)
+        w, h = int(dim_split[0]), int(dim_split[1])
+    else:
+        h = w = int(yolo_dim)
+    if h % 32 != 0 or w % 32 != 0:
+        raise SystemExit('ERROR: bad yolo_dim (%s)!' % yolo_dim)
+
+    trt_yolo = TrtYOLO(args.yolo_model, (h, w), args.category_num, args.letter_box)
+
+    open_window(
+        WINDOW_NAME, 'Camera TensorRT YOLO Demo',
+        cam.img_width, cam.img_height)
+    vis = BBoxVisualization(cls_dict)
+    loop_and_detect(cam, trt_yolo, conf_th=0.3, vis=vis)
+
+    cam.release()
+    cv2.destroyAllWindows()
+    
+           
+WINDOW_NAME = 'Demo'
+args = parse_args()
+
+with open('./trt_pose/tasks/human_pose/human_pose.json', 'r') as f:
+    human_pose = json.load(f)
+
+topology = trt_pose.coco.coco_category_to_topology(human_pose)
+
+num_parts = len(human_pose['keypoints'])
+num_links = len(human_pose['skeleton'])
+
+if 'resnet' in args.pose_model:
+    print('------ model = resnet--------')
+    MODEL_WEIGHTS = './trt_pose/tasks/human_pose/resnet18_baseline_att_224x224_A_epoch_249.pth'
+    OPTIMIZED_MODEL = './trt_pose/tasks/human_pose/resnet18_baseline_att_224x224_A_epoch_249_trt.pth'
+    model = trt_pose.models.resnet18_baseline_att(num_parts, 2 * num_links).cuda().eval()
+    WIDTH = 224
+    HEIGHT = 224
+
+else:    
+    print('------ model = densenet--------')
+    MODEL_WEIGHTS = './trt_pose/tasks/human_pose/densenet121_baseline_att_256x256_B_epoch_160.pth'
+    OPTIMIZED_MODEL = './trt_pose/tasks/human_pose/densenet121_baseline_att_256x256_B_epoch_160_trt.pth'
+    model = trt_pose.models.densenet121_baseline_att(num_parts, 2 * num_links).cuda().eval()
+    WIDTH = 256
+    HEIGHT = 256
+
+data = torch.zeros((1, 3, HEIGHT, WIDTH)).cuda()
+if os.path.exists(OPTIMIZED_MODEL) == False:
+    print('-- Converting TensorRT models. This may takes several minutes...')
+    model.load_state_dict(torch.load(MODEL_WEIGHTS))
+    model_trt = torch2trt.torch2trt(model, [data], fp16_mode=True, max_workspace_size=1<<25)
+    torch.save(model_trt.state_dict(), OPTIMIZED_MODEL)
+
+model_trt = TRTModule()
+model_trt.load_state_dict(torch.load(OPTIMIZED_MODEL))
+
+t0 = time.time()
+torch.cuda.current_stream().synchronize()
+for i in range(50):
+    y = model_trt(data)
+torch.cuda.current_stream().synchronize()
+t1 = time.time()
+
+print(50.0 / (t1 - t0))
+
+mean = torch.Tensor([0.485, 0.456, 0.406]).cuda()
+std = torch.Tensor([0.229, 0.224, 0.225]).cuda()
+device = torch.device('cuda')
+
+parse_objects = ParseObjects(topology)
+draw_objects = DrawObjects(topology)
+
+fontname = '/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc'
+fnt = PIL.ImageFont.truetype(fontname, 45)
+
+
+if __name__ == '__main__':
+    main() 
